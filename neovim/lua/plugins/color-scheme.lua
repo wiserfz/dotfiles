679b083e45b8d847c4145c29ab486c2107e9a57b
wiser-fz <mailfz810@163.com>
2023-04-19 11:38:32 +0800

    Modify color scheme setting

--- neovim/lua/plugins/color-scheme.lua
+++ neovim/lua/plugins/color-scheme.lua
@@ -19,8 +19,26 @@ end
 
 scheme.setup({
   keywordStyle = { italic = false, bold = true },
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+      NormalFloat = { bg = "none" },
+      FloatBorder = { bg = "none" },
+      FloatTitle = { bg = "none" },
+
+      -- Save an hlgroup with dark background and dimmed foreground
+      -- so that you can use it where your still want darker windows.
+      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+      -- Popular plugins that open floats will link to NormalFloat by default;
+      -- set their background accordingly if you wish to keep them dark and borderless
+      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    }
+  end,
 })
-vim.cmd("colorscheme kanagawa")
+vim.cmd("colorscheme kanagawa-wave")
 
 -- Highlight colors
 vim.cmd([[
