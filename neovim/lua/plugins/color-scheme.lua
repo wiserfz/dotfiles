dc6a440955960c84a37c4405c43d4e7b1c6efaa5
wiser-fz <mailfz810@163.com>
2023-05-16 17:53:17 +0800

    Add MatchParen for color-scheme

--- neovim/lua/plugins/color-scheme.lua
+++ neovim/lua/plugins/color-scheme.lua
@@ -48,4 +48,5 @@ vim.cmd([[
   hi GitSignsChange guibg=#193549 guifg=#ffc600
   hi GitSignsDelete guibg=#193549 guifg=#ff2600
   hi ColorColumn guifg=NONE guibg=#204563 gui=NONE
+  hi MatchParen cterm=bold gui=bold guifg=#FD9F4E guibg=#7B7C7C
 ]])
