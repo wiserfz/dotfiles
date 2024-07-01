578beb8c6e9341ff29933a4ba7d42dac67b7fbaf
wiser-fz <mailfz810@163.com>
2024-07-01 11:15:35 +0800

    Refactor fish shell prompt function

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,153 +1,156 @@
 function fish_prompt
     set -l __last_command_exit_status $status
 
-    if not set -q __CONFIG_DIR
-        set __CONFIG_DIR ~/.config
+    if not set -q __config_dir
+        set __config_dir ~/.config
     end
     # Colors
     # Reset
-    set ResetColor (set_color normal) # Text Reset
+    set reset_color (set_color normal) # Text Reset
 
     # Regular Colors
-    set Red (set_color red) # Red
-    set Yellow (set_color yellow) # Yellow
-    set Blue (set_color blue) # Blue
-    set White (set_color white)
-    set Green (set_color green)
+    set red (set_color red) # red
+    set yellow (set_color yellow) # yellow
+    set blue (set_color blue) # blue
+    set white (set_color white)
+    set green (set_color green)
 
     # Bold
-    set BRed (set_color -o red)
-    set BGreen (set_color -o green) # Green
-    set BWhite (set_color -o white)
-    set BBlue (set_color -o blue)
+    set bold_red (set_color -o red)
+    set bold_green (set_color -o green) # Green
+    set bold_white (set_color -o white)
+
+    set git_changed_color (set_color -o ffc600)
+    set git_add_color (set_color -o 3ad900)
+    set git_delete_color (set_color -o ff2600)
+    set git_remote_color (set_color -o 009aa6)
 
     # High Intensty
-    set IBlack (set_color -o black) # Black
+    set intensty_black (set_color -o black) # Black
 
     # Bold High Intensty
-    set Magenta (set_color -o purple) # Purple
+    set magenta (set_color -o purple) # Purple
 
-    set BCyan (set_color -o cyan)
+    set bold_cyan (set_color -o cyan)
 
     # rust
-    set BOrange (set_color -o F05A1B)
+    set bold_orange (set_color -o F05A1B)
     # go
-    set BBrBlue (set_color -o 4DD1FF)
+    set br_blue (set_color -o 4DD1FF)
     # python
-    set BBrGreen (set_color -o BFD55D)
+    set br_green (set_color -o BFD55D)
 
     # Default values for the appearance of the prompt. Configure at will.
-    set PROMPT_PREFIX "["
-    set PROMPT_SUFFIX "]"
-    set PROMPT_SEPARATOR "|"
-    set PROMPT_
+    set prompt_prefix "["
+    set prompt_suffix "]"
+    set prompt_separator "|"
 
     # Various variables you might want for your PS1 prompt instead
     # set Time (date +%R)
-    set PathShort (pwd|sed "s=$HOME=~=")
+    set path_short (pwd|sed "s=$HOME=~=")
 
-    set PROMPT_START "$Yellow$PathShort$ResetColor"
-    set PROMPT_END " \n$BWhite\$ $ResetColor"
+    set prompt_start "$yellow$path_short$reset_color"
+    set prompt_end " \n$bold_white\$ $reset_color"
     if test $__last_command_exit_status != 0
-        set PROMPT_END " \n$BRed\$ $ResetColor"
+        set prompt_end " \n$bold_red\$ $reset_color"
     end
 
     # pyenv
-    set PROMPT_PYENV ""
+    set prompt_pyenv ""
     set py_env $VIRTUAL_ENV
-    if test -n "$py_env"
-        set PYTHON "🐍"
-        set PYTHON_VERSION (pyenv version-name)
-        set PROMPT_PYENV " $PYTHON $BBrGreen$PYTHON_VERSION$ResetColor"
+    set requirements "requirements.txt"
+    if test -n "$py_env" || test -f "$requirements"
+        set python_icon "🐍"
+        set python_version (pyenv version-name)
+        set prompt_pyenv " $python_icon $br_green$python_version$reset_color"
     end
 
     # rust
-    set PROMPT_RUST ""
-    set CARGO_CONFIG "Cargo.toml"
-    if test -f "$CARGO_CONFIG"
-        set RUST "🦀"
-        set RUST_VERSION (rustc --version | string split ' ' -f 2)
-        set PROMPT_RUST " $RUST $BOrange$RUST_VERSION$ResetColor"
+    set prompt_rust ""
+    set cargo_config "Cargo.toml"
+    if test -f "$cargo_config"
+        set rust_icon "🦀"
+        set rust_version (rustc --version | string split ' ' -f 2)
+        set prompt_rust " $rust_icon $bold_orange$rust_version$reset_color"
     end
 
     # go
-    set PROMPT_GO ""
-    set GO_CONFIG "go.mod"
-    if test -f "$GO_CONFIG"
-        set GO "ʕ◔ϖ◔ʔ"
-        set GO_VERSION (go version | string split ' ' -f 3 | string trim -c "go")
-        set PROMPT_GO " $BBrBlue$GO $GO_VERSION$ResetColor"
+    set prompt_go ""
+    set go_config "go.mod"
+    if test -f "$go_config"
+        set go_icon "ʕ◔ϖ◔ʔ"
+        set go_version (go version | string split ' ' -f 3 | string trim -c "go")
+        set prompt_go " $br_blue$go_icon $go_version$reset_color"
     end
 
-    set -e __CURRENT_GIT_STATUS
-    set gitstatus "$__CONFIG_DIR/gitstatus.py"
+    set -e __current_git_status
+    set gitstatus "$__config_dir/gitstatus.py"
 
-    set _GIT_STATUS (python $gitstatus)
-    set __CURRENT_GIT_STATUS $_GIT_STATUS
+    set __current_git_status (python $gitstatus)
 
-    set __CURRENT_GIT_STATUS_PARAM_COUNT (count $__CURRENT_GIT_STATUS)
+    set __current_git_status_param_count (count $__current_git_status)
 
-    set GIT_PROMPT_BRANCH "$Magenta"
-    set GIT_PROMPT_STAGED "$Red● "
-    set GIT_PROMPT_CONFLICTS "$Red✖ "
-    set GIT_PROMPT_CHANGED "$Blue✚ "
-    set GIT_PROMPT_REMOTE " "
-    set GIT_PROMPT_UNTRACKED "…"
-    set GIT_PROMPT_STASHED "⚑ "
-    set GIT_PROMPT_CLEAN "$BGreen✔"
+    set git_prompt_branch "$magenta"
+    set git_prompt_staged "$git_add_color● "
+    set git_prompt_conflicts "$git_delete_color✖ "
+    set git_prompt_changed "$git_changed_color✚ "
+    set git_prompt_remote "$git_remote_color 󰅡 "
+    set git_prompt_untracked "$bold_cyan "
+    set git_prompt_stashed "$intensty_black⚑ "
+    set git_prompt_clean "$bold_green✔"
 
-    if not test 0 -eq $__CURRENT_GIT_STATUS_PARAM_COUNT
-        set GIT_BRANCH $__CURRENT_GIT_STATUS[1]
-        set GIT_REMOTE "$__CURRENT_GIT_STATUS[2]"
-        if contains "." "$GIT_REMOTE"
-            set -e GIT_REMOTE
+    if test 0 -ne $__current_git_status_param_count
+        set git_branch "$__current_git_status[1]"
+        set git_remote "$__current_git_status[2]"
+        if contains "." "$git_remote"
+            set -e git_remote
         end
-        set GIT_STAGED $__CURRENT_GIT_STATUS[3]
-        set GIT_CONFLICTS $__CURRENT_GIT_STATUS[4]
-        set GIT_CHANGED $__CURRENT_GIT_STATUS[5]
-        set GIT_UNTRACKED $__CURRENT_GIT_STATUS[6]
-        set GIT_STASHED $__CURRENT_GIT_STATUS[7]
-        set GIT_CLEAN $__CURRENT_GIT_STATUS[8]
+        set git_staged $__current_git_status[3]
+        set git_conflicts $__current_git_status[4]
+        set git_changed $__current_git_status[5]
+        set git_untracked $__current_git_status[6]
+        set git_stashed $__current_git_status[7]
+        set git_clean $__current_git_status[8]
     end
 
-    if test -n "$__CURRENT_GIT_STATUS"
-        set STATUS " $PROMPT_PREFIX$GIT_PROMPT_BRANCH$GIT_BRANCH$ResetColor"
+    if test -n "$__current_git_status"
+        set git_status " $prompt_prefix$git_prompt_branch$git_branch$reset_color"
 
-        if set -q GIT_REMOTE
-            set STATUS "$STATUS$GIT_PROMPT_REMOTE$GIT_REMOTE$ResetColor"
+        if set -q git_remote
+            set git_status "$git_status$git_prompt_remote$git_remote$reset_color"
         end
 
-        set STATUS "$STATUS$PROMPT_SEPARATOR"
+        set git_status "$git_status$prompt_separator"
 
-        if [ $GIT_STAGED != 0 ]
-            set STATUS "$STATUS$GIT_PROMPT_STAGED$GIT_STAGED$ResetColor"
+        if [ $git_staged != 0 ]
+            set git_status "$git_status$git_prompt_staged$git_staged$reset_color"
         end
 
-        if [ $GIT_CONFLICTS != 0 ]
-            set STATUS "$STATUS$GIT_PROMPT_CONFLICTS$GIT_CONFLICTS$ResetColor"
+        if [ $git_conflicts != 0 ]
+            set git_status "$git_status$git_prompt_conflicts$git_conflicts$reset_color"
         end
 
-        if [ $GIT_CHANGED != 0 ]
-            set STATUS "$STATUS$GIT_PROMPT_CHANGED$GIT_CHANGED$ResetColor"
+        if [ $git_changed != 0 ]
+            set git_status "$git_status$git_prompt_changed$git_changed$reset_color"
         end
 
-        if [ "$GIT_UNTRACKED" != 0 ]
-            set STATUS "$STATUS$GIT_PROMPT_UNTRACKED$GIT_UNTRACKED$ResetColor"
+        if [ "$git_untracked" != 0 ]
+            set git_status "$git_status$git_prompt_untracked$git_untracked$reset_color"
         end
 
-        if [ "$GIT_STASHED" != 0 ]
-            set STATUS "$STATUS$GIT_PROMPT_STASHED$GIT_STASHED$ResetColor"
+        if [ "$git_stashed" != 0 ]
+            set git_status "$git_status$git_prompt_stashed$git_stashed$reset_color"
         end
 
-        if [ "$GIT_CLEAN" = 1 ]
-            set STATUS "$STATUS$GIT_PROMPT_CLEAN"
+        if [ "$git_clean" = 1 ]
+            set git_status "$git_status$git_prompt_clean"
         end
 
-        set STATUS "$STATUS$ResetColor$PROMPT_SUFFIX"
+        set git_status "$git_status$reset_color$prompt_suffix"
 
-        set PS1 "$PROMPT_START$STATUS$PROMPT_PYENV$PROMPT_GO$PROMPT_RUST$PROMPT_END"
+        set PS1 "$prompt_start$git_status$prompt_pyenv$prompt_go$prompt_rust$prompt_end"
     else
-        set PS1 "$PROMPT_START$PROMPT_PYENV$PROMPT_GO$PROMPT_RUST$PROMPT_END"
+        set PS1 "$prompt_start$prompt_pyenv$prompt_go$prompt_rust$prompt_end"
     end
 
     echo -e $PS1
