43b30b9d60e14609dd87cdb19f04da65395f4065
wiser-fz <wiserfz810@gmail.com>
2025-04-11 18:00:05 +0800

    Update fold provider and lualine status

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,6 +1,7 @@
 local lualine = require("lualine")
 local diagnostics_icon = require("util").diagnostics
 local lazy_status = require("lazy.status")
+local devicons = require("nvim-web-devicons")
 
 --@param name string @The name of the highlight group
 --@return function @A function that returns the foreground color of the highlight group
@@ -10,57 +11,6 @@ local function fg(name)
     local hl = vim.api.nvim_get_hl_by_name(name, true)
     return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
   end
-end
-
---@return table @A lualine component that displays the status of Code Companion requests
-local function code_companion_status()
-  local code_companion = require("lualine.component"):extend()
-  code_companion.processing = false
-  code_companion.spinner_index = 1
-
-  local spinner_symbols = {
-    "⠋",
-    "⠙",
-    "⠹",
-    "⠸",
-    "⠼",
-    "⠴",
-    "⠦",
-    "⠧",
-    "⠇",
-    "⠏",
-  }
-  local spinner_symbols_len = 10
-
-  -- Initializer
-  function code_companion:init(options)
-    code_companion.super.init(self, options)
-
-    local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
-    vim.api.nvim_create_autocmd({ "User" }, {
-      pattern = "CodeCompanionRequest*",
-      group = group,
-      callback = function(request)
-        if request.match == "CodeCompanionRequestStarted" then
-          self.processing = true
-        elseif request.match == "CodeCompanionRequestFinished" then
-          self.processing = false
-        end
-      end,
-    })
-  end
-
-  -- Function that runs every time statusline is updated
-  function code_companion:update_status()
-    if self.processing then
-      self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-      return spinner_symbols[self.spinner_index]
-    else
-      return nil
-    end
-  end
-
-  return code_companion
 end
 
 local M = {}
@@ -82,7 +32,11 @@ function M.setup()
       lualine_a = {
         {
           "mode",
+          fmt = function(str)
+            return " " .. str
+          end,
           separator = { left = "", right = "" },
+          color = { gui = "bold" },
           -- padding = { left = 1, right = 1 },
         },
       },
@@ -91,7 +45,15 @@ function M.setup()
         {
           "filename",
           path = 1,
+          fmt = function(str)
+            local name = str
+            local extension = vim.fn.fnamemodify(name, ":e")
+            local icon = devicons.get_icon(name, extension, { default = true })
+            return icon .. " " .. str
+          end,
         },
+      },
+      lualine_x = {
         {
           "diagnostics",
           symbols = {
@@ -101,8 +63,6 @@ function M.setup()
             hint = diagnostics_icon.Hint,
           },
         },
-      },
-      lualine_x = {
         {
           function()
             local style = vim.bo.expandtab and "Spaces" or "Tab Size"
@@ -112,7 +72,7 @@ function M.setup()
           cond = function()
             return vim.bo.filetype ~= ""
           end,
-          color = fg("String"),
+          color = fg("Number"),
         },
         {
           "encoding",
@@ -121,12 +81,10 @@ function M.setup()
           end,
           color = { fg = "#a9a1e1" },
         },
-        { "filetype", color = { fg = "#ECBE7B" } },
-        { code_companion_status },
         {
           lazy_status.updates,
           cond = lazy_status.has_updates,
-          color = fg("Special"),
+          color = fg("WarningMsg"),
         },
       },
       lualine_y = {
