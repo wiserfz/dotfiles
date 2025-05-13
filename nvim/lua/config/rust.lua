25a5eb15144a7fa333ca99a49d72f1da5c918405
wiser-fz <wiserfz810@gmail.com>
2025-05-13 10:07:16 +0800

    Refactor rust lsp configruation

--- nvim/lua/config/rust.lua
+++ nvim/lua/config/rust.lua
@@ -8,79 +8,98 @@ local mason_path = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/"
 local codelldb_path = mason_path .. "codelldb/extension/adapter/codelldb"
 local liblldb_path = mason_path .. "codelldb/extension/lldb/lib/liblldb.dylib"
 
+-- disable features for some projects
+local disable_features_project = {
+  "kafka-tower",
+}
+
 local M = {}
 
 function M.setup()
-  vim.g.rustaceanvim = {
-    server = {
-      capabilities = lsp.get_capabilities(),
-      on_attach = function(client, bufnr)
-        lsp.on_attach(client, bufnr)
+  ---@return rustaceanvim.Opts
+  vim.g.rustaceanvim = function()
+    ---@type rustaceanvim.Opts
+    local opts = {
+      server = {
+        capabilities = lsp.get_capabilities(),
+        on_attach = function(client, bufnr)
+          lsp.on_attach(client, bufnr)
 
-        wk.add({
-          noremap = true,
-          silent = true,
-          buffer = bufnr,
+          wk.add({
+            noremap = true,
+            silent = true,
+            buffer = bufnr,
 
-          {
-            "<leader>ca",
-            function()
-              vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-            end,
-            desc = "Rustaceanvim code action",
-          },
-          {
-            "K",
-            function()
-              vim.cmd.RustLsp({ "hover", "actions" })
-            end,
-            desc = "Rustaceanvim hover actions",
-          },
-        })
-      end,
-      default_settings = {
-        ["rust-analyzer"] = {
-          imports = {
-            granularity = {
-              group = "module",
+            {
+              "<leader>ca",
+              function()
+                vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+              end,
+              desc = "Rustaceanvim code action",
             },
-            prefix = "self",
-          },
-          checkOnSave = true,
-          check = {
-            command = "clippy",
-          },
-          cargo = {
-            buildScripts = {
+            {
+              "K",
+              function()
+                vim.cmd.RustLsp({ "hover", "actions" })
+              end,
+              desc = "Rustaceanvim hover actions",
+            },
+          })
+        end,
+        default_settings = {
+          ["rust-analyzer"] = {
+            imports = {
+              granularity = {
+                group = "module",
+              },
+              prefix = "self",
+            },
+            checkOnSave = true,
+            check = {
+              command = "clippy",
+            },
+            procMacro = {
               enable = true,
+              attributes = {
+                enable = true,
+              },
             },
-            features = "all",
-          },
-          procMacro = {
-            enable = true,
-            attributes = {
-              enable = true,
-            },
-          },
-          files = {
-            excludeDirs = {
-              ".git",
-              ".gitlab-ci",
-              ".idea",
-              ".vscode",
-              ".settings",
-              "node_modules",
-              "target",
+            files = {
+              exclude = {
+                ".git",
+                ".gitlab-ci",
+                ".idea",
+                ".vscode",
+                ".settings",
+                "node_modules",
+                "target",
+              },
             },
           },
         },
       },
-    },
-    -- for debugger
-    dap = {
-      adapter = rust_config.get_codelldb_adapter(codelldb_path, liblldb_path),
-    },
-  }
+      -- for debugger
+      dap = {
+        adapter = rust_config.get_codelldb_adapter(codelldb_path, liblldb_path),
+      },
+    }
+
+    local cargo_options = {
+      buildScripts = {
+        enable = true,
+      },
+      features = "all",
+    }
+
+    local project_name = vim.fs.basename(vim.fn.getcwd())
+    if vim.list_contains(disable_features_project, project_name) then
+      cargo_options.features = nil
+    end
+
+    opts.server.default_settings["rust-analyzer"].cargo = cargo_options
+
+    return opts
+  end
 end
 
 return M
