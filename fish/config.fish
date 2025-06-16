232fa20313dc11fe565ff516f27587092590355b
wiser-fz <wiserfz810@gmail.com>
2025-06-16 14:05:45 +0800

    Use orbstack instead of docker desktop

--- fish/config.fish
+++ fish/config.fish
@@ -66,3 +66,7 @@ zoxide init fish | source
 
 # setup with atuin instead of fzf
 atuin init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
